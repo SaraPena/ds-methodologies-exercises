bbe37e7ec7b559b3af711aaa61df8f835fbb6b4c
SaraPena <pena.e.sara@gmail.com>
2019-10-14 10:35:41 -0500

    Create regression/evaluate.py

---
+++ regression/evaluate.py
@@ -0,0 +1,19 @@
+import pandas as pd 
+import numpy as np 
+import seaborn as sns
+from scipy import stats
+from statsmodels.formula.api import ols
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score, explained_variance_score
+from sklearn.feature_selection import f_regression
+from math import sqrt
+import matplotlib.pyplot as plt 
+%matplotlib inline
+import warnings
+warnings.filterwarnings('ignore')
+
+
+
+
+
+
