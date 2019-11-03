90f7083625cd5f02df27a4f4e7bfe8ef85eedf80
SaraPena <pena.e.sara@gmail.com>
2019-11-02 20:48:23 -0500

    Create wrangle_zillow.py file

---
+++ clustering/wrangle_zillow.py
@@ -0,0 +1,19 @@
+# ignore warnings
+import warnings
+warnings.filterwarnings("ignore")
+
+# Wrangling
+import pandas as pd 
+import numpy as np 
+
+# Exploring
+import scipy.stats as stats
+import matplotlib.pyplot as plt 
+import seaborn as sns 
+
+# default pandas decimal number display format.
+pd.options.display.float_format = '{:20,.2f}'.format
+
+import acquire
+
+
