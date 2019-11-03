bec741d2a74e6403ad8a7ec10f2003fc6b9466ed
SaraPena <pena.e.sara@gmail.com>
2019-11-02 20:47:37 -0500

    Update acquire.py

--- clustering/acquire.py
+++ clustering/acquire.py
@@ -12,25 +12,30 @@ def get_db_url(db):
 # Example:
 # url = get_db_url('zillow')
 
-def get_data_from_mysql(query, db):
+def get_data_from_mysql():
     """use pd.read_sql to get data from query variable that is a syntatically correct sql query, and db tha is an existing database on your sql host
         \n returns dataframe of query """
-    return pd.read_sql(query, get_db_url(db))
+        
+    query = """SELECT logerror, transactiondate, p.*
+    FROM predictions_2017 p_17
+    JOIN 
+        (SELECT
+        parcelid, Max(transactiondate) as tdate
+        FROM predictions_2017
+        GROUP BY parcelid )as sq1
+    		on (sq1.parcelid=p_17.parcelid and sq1.tdate = p_17.transactiondate)
+    JOIN properties_2017 p on p_17.parcelid=p.parcelid
+    WHERE p.latitude IS NOT NULL and p.longitude IS NOT NULL and YEAR(p_17.transactiondate) = '2017';"""
+
+    db = 'zillow'
+    
+    df = pd.read_sql(query, get_db_url(db))
+
+    return df
     
 # Example:
-# query = """SELECT p_17.parcelid, logerror, transactiondate, p.*
-#     FROM predictions_2017 p_17
-#     JOIN 
-#         (SELECT
-#         parcelid, Max(transactiondate) as tdate
-#         FROM predictions_2017
-#         GROUP BY parcelid )as sq1
-#     		on (sq1.parcelid=p_17.parcelid and sq1.tdate = p_17.transactiondate)
-#     JOIN properties_2017 p on p_17.parcelid=p.parcelid
-#     WHERE p.latitude IS NOT NULL and p.longitude IS NOT NULL and YEAR(p_17.transactiondate) = '2017';"""
-
-# db = 'zillow'
-# get_data_from_mysql(query, db)
+# df = get_data_from_mysql()
+# df.head()
 
 
 
