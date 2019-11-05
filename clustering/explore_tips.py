b0bef4bd1c6b5df67c5305a8cc96aed49563f681
SaraPena <pena.e.sara@gmail.com>
2019-11-05 12:04:38 -0600

    Update explore_tips.py file chi^2 test/graph

--- clustering/explore_tips.py
+++ clustering/explore_tips.py
@@ -18,6 +18,7 @@ from sklearn.preprocessing import StandardScaler
 from math import sqrt
 import scipy.stats as stats
 
+import matplotlib
 import matplotlib.pyplot as plt
 from matplotlib import cm
 from sklearn.model_selection import learning_curve
@@ -25,7 +26,7 @@ from sklearn.model_selection import learning_curve
 
 #pd.options.display.float_format = '{:20, .2f}'.format
 
-# You can 
+# Load the dataset:
 df = sns.load_dataset('tips')
 
 
@@ -70,5 +71,40 @@ observed = pd.DataFrame({'no_smoker': no_smoker, 'yes_smoker': yes_smoker})
 
 stats.chi2_contingency(observed)
 
+labels = ['Dinner', 'Lunch']
+no_smoker_counts = list(observed.no_smoker)
+yes_smoker_counts = list(observed.yes_smoker)
+
+x = np.arange(len(labels))
+width = 0.35
+
+fig, ax = plt.subplots()
+rect1 = ax.bar(x-width/2, no_smoker_counts, width, label = 'no_smoker')
+rect2 =ax.bar(x+width/2, yes_smoker_counts, width, label = 'yes_smoker')
+
+ax.set_ylabel('count')
+ax.set_title('Count of Smokers by Time')
+ax.set_xticks(x)
+ax.set_xticklabels(labels)
 # With a higher p value of .47 we can say that smoking is independent of time of day 
-observed.boxplot()
+def autolabel(rects, xpos='center'):
+    """
+    Attach a text label above each bar in *rects*, displaying its height.
+
+    *xpos* indicates which side to place the text w.r.t. the center of
+    the bar. It can be one of the following {'center', 'right', 'left'}.
+    """
+
+    xpos = xpos.lower()  # normalize the case of the parameter
+    ha = {'center': 'center', 'right': 'left', 'left': 'right'}
+    offset = {'center': 0.5, 'right': 0.57, 'left': 0.43}  # x_txt = x + w*off
+
+    for rect in rects:
+        height = rect.get_height()
+        ax.text(rect.get_x() + rect.get_width()*offset[xpos], 1.01*height,
+                '{}'.format(height), ha=ha[xpos], va='bottom')
+
+autolabel(rect1, "center")
+autolabel(rect2, "center")
+
+
