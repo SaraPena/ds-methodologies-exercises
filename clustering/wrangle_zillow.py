5b0cfc4628d660e9305ea465a5d5905d3fa00bb0
SaraPena <pena.e.sara@gmail.com>
2019-11-06 12:20:25 -0600

    Update wrangle_zillow.py file

--- clustering/wrangle_zillow.py
+++ clustering/wrangle_zillow.py
@@ -22,10 +22,9 @@ def wrangle_zillow_data():
     df = prepare.zillow_single_unit(df)
     df = prepare.remove_columns(df,['finishedsquarefeet12','fullbathcnt', 'unitcnt','structuretaxvaluedollarcnt','landtaxvaluedollarcnt', 'assessmentyear', 'propertyzoningdesc'])
     df = prepare.handle_missing_values(df)
-    df.dropna(inplace=True)
     return df
 
-wrangle_zillow_data()
-
+# wrangle_zillow_data().info()
+# wrangle_zillow_data().fips.value_counts()
 
 
